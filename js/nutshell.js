89ef2aaf1f1980001c7b9ce49441b8f4bbbb1f8f
DocumentList <54522267+DocumentList@users.noreply.github.com>
2019-09-16 19:28:38 +0800

    响应式
    响应式网站

---
+++ js/nutshell.js
@@ -0,0 +1,26 @@
+$(function(){
+	
+	$('.name-hea>li').hover(function(){
+		var i = $(this).index()
+		console.log(i)
+		$('.name-img>li').eq(i).show().siblings().hide()
+	})
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+})
